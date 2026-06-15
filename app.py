692f163bff8ccd5b1b59592b3f1eccbc1b65fe19
neri-usagi <qjmg1234@gmail.com>
2026-06-15 14:10:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
-from langchain.chains import RetrievalQA
 import tempfile
 import os
 
